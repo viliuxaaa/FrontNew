8f71bbcbff6f4bf2ab939113ccd456ff2dd603aa
ed <stanke367@gmail.com>
2023-08-07 21:37:42 +0300

    prideta LT-RU btn ir koreguotas navbaras

--- src/Layout/Navbar/NavBar.js
+++ src/Layout/Navbar/NavBar.js
@@ -3,14 +3,15 @@ import { FaSearch } from "react-icons/fa";
 import { CgUser } from "react-icons/cg";
 
 function NavBar() {
-    const hover = "hover:text-textHover transitions text-text";
+    const hover = "hover:text-cyan-800 transition hover:scale-105 transitions text-text";
     const Hover = ({isActive}) => (isActive ? "text-custom" : hover)
+    
     return (
         <>
-        <div className="bg-main shadow-md md:sticky top-0 z-20">
-            <div className="container mx-auto py-6 px-2 lg:grid gap-10 grid-cols-7 justify-between items-center">
+        <div className="bg-main shadow-md md:sticky top-0 z-20 h-[165px] lg:h-full">
+            <div className="container mx-auto py-6 lg:px-2 lg:grid gap-10 grid-cols-7 justify-between items-center">
                 {/* Logo */}
-                <div className="col-span-1 block pb-1">
+                <div className="col-span-1 block pb-1 scale-110 hover:scale-125 transition w-[250px] mx-auto lg:w-full">
                     <Link to="/">
                         <img 
                         src="/images/logo.png" 
@@ -36,7 +37,7 @@ function NavBar() {
                     </form>
                 </div>
                 {/* menus */}
-                <div className="col-span-3 font-medium text-sm xl:gap-14 2xl:gap-24 justify-between flex items-center">
+                <div className="col-span-3 pt-1 lg:pt-0 font-medium text-sm xl:gap-14 2xl:gap-24 justify-between flex items-center">
                   <NavLink to="/upload" className={Hover}>Add Poster</NavLink>  
                   <NavLink to="/posters" className={Hover}>Skelbimai</NavLink>
                   <NavLink to="/dashboard" className={Hover}>Dash</NavLink>   
@@ -44,6 +45,15 @@ function NavBar() {
                     <CgUser className="w-8 h-8" />
                   </NavLink> 
                 </div>
+            </div>
+            <div className="absolute flex font-medium text-text w-18 h-7 right-7 top-4">
+                <button className="w-9 h-7 transition justify-center items-center hover:scale-105 hover:text-teal-600">
+                    LT
+                </button>
+                <span className="pt-[2px]">|</span>
+                <button className="w-9 h-7 transition justify-center items-center hover:scale-105 hover:text-teal-600">
+                    RU
+                </button>
             </div>
         </div>
         </>
