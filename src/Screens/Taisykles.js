b3936ed92b174edc350c0c0efc6d14006854f20e
Dalius <dalius.rac@gmail.com>
2023-08-11 10:41:28 +0300

    Isversti kontaktai politika ir taisykles

--- src/Screens/Taisykles.js
+++ src/Screens/Taisykles.js
@@ -1,72 +1,38 @@
-import React from 'react'
-import Layout from '../Layout/Layout'
+import React from "react";
+import Layout from "../Layout/Layout";
+import { useTranslation } from "react-i18next";
 
 function Taisykles() {
+  const [t, i18n] = useTranslation("global");
   return (
     <Layout>
-         <div className='flex-colo gap-8 w-full min-h-screen text-text lg:py-20 py-10 px-6'>
-                <div className="mx-auto bg-subMain p-6 shadow-md rounded-lg">
-                    <h1 className="text-2xl font-bold mb-4">NAUDOJIMO TAISYKLĖS (redakcija 2023-08-10)</h1>
-                    <h2 className="text-xl font-semibold mb-2">1. TAISYKLIŲ TAIKYMAS IR AIŠKINIMAS</h2>
-                    <p className="mb-4">
-                    1.1. Šis dokumentas yra sutartis tarp UAB "Įdomios technologijos" (juridinio asmens kodas – 387661902, PVM mokėtojo kodas – LT228176005, registruotos buveinės adresas Pylimo g. 17A, 03234 Vilnius, Lietuva, el. pašto adresas – <a href="mailto:info@fake.lt" className="text-blue-600 underline">info@neskelbiu.lt</a>, tel. nr.: +370 655 87463) (toliau – neskelbiu.TECH) ir Jūsų (toliau – Klientas arba Jūs) dėl naudojimosi interneto svetaine <a href="https://www.neskelbiu.lt" className="text-blue-600 underline">www.neskelbiu.lt</a> ir (ar) fantazija.TECH mobiliąja aplikacija, kurią galima atsiųsti iš „Apple App Store“ ir „Google Play“  (toliau – Svetainė).
-                    </p>
-                    <p className="mb-4">
-                    1.2. Naudodamiesi Svetaine Jūs patvirtinate, kad esate susipažinęs su šios sutarties sąlygomis ir taisyklėmis (toliau – Taisyklės), sutinkate su Taisyklėmis ir įsipareigojate jų laikytis.
-                    </p>
-                    <p className="mb-4">
-                    1.3. Naudojimasis Svetaine šių Taisyklių prasme apima visų ir bet kokių veiksmų atlikimą, Klientui prisijungus prie Svetainės naudojantis interneto ryšiu. Šių Taisyklių prasme, paslaugos apima visus ir bet kokius Kliento veiksmus, kuriuos jis gali atlikti Svetainėje, įskaitant skelbimų skelbimą, skelbiamos informacijos skaitymą, klausimų, atsiliepimų ir komentarų rašymą, tiesioginių (online) žinučių rašymą, bet kokio pobūdžio informacijos ir (ar) duomenų pateikimą, siuntimą bei gavimą, tačiau tuo neapsiribojant (toliau – Paslaugos). Klientas bet kokiu būdu ir forma naudodamasis Svetaine ir (ar) Paslaugomis, besąlygiškai ir neatšaukiamai įsipareigoja laikytis visų šiose Taisyklėse nustatytų reikalavimų. Jei Klientas nesutinka besąlygiškai bei neatšaukiamai prisiimti ir laikytis visų šiose Taisyklėse nustatytų įsipareigojimų, jis neturi teisės naudotis jokiomis Paslaugomis ir (ar) Svetaine.
-                    </p>
-                    <p className="mb-4">
-                    1.4. Dalis Svetainėje teikiamų Paslaugų yra teikiamos Klientui už atlygį (toliau – Mokamos Paslaugos). Mokamos Paslaugos yra apibūdintos ir nurodytos Svetainėje, Jų kaina nurodyta konkrečios Mokamos Paslaugos užsakymo puslapyje. Toliau šiose Taisyklėse nuoroda į „Paslaugas“ apims visas Paslaugas, įskaitant ir Mokamas Paslaugas, o nuoroda į „Mokamas Paslaugas“ apims tik tas Paslaugas už kurias Klientas moka užmokestį.
-                    </p>
-                    <p className="mb-4">
-                    1.5. Visi Klientai yra skirstomi į verslo ir privačius klientus ir apibrėžiami kaip nurodyta žemiau:
-                    </p>
-                    <p className="mb-4">
-                    1.5.1. Verslo Klientams priskiriami fiziniai ir juridiniai asmenys, kurie, atsižvelgiant į įkeltų skelbimų kiekį, skelbimų turinį (pavyzdžiui, aprašymo, nuotraukų), kitas turimas susijusias paskyras, Paslaugomis naudojasi prekybos, verslo, amato, profesijos tikslais (pavyzdžiui, vykdo tęstinę prekybą, teikia paslaugas, parduoda veislinių gyvūnų jauniklius, vykdo ūkinę veiklą, superka daiktus ir pan.) (toliau – Verslininkai);
-                    </p>
-                    <p className="mb-4">
-                    1.5.2. Privatiems Klientams priskiriami fiziniai ir juridiniai asmenys, kurie Paslaugomis naudojasi su savo verslu, amatu ar profesija nesusijusiais tikslais (vartojimo tikslais) (toliau – Privatūs klientai).
-                    </p>
-                    <p className="mb-4">
-                    1.6. Be šių Taisyklių santykiams tarp Kliento ir neskelbiu.LT reguliuoti taikomos Lietuvos Respublikos teisės aktų nuostatos, specialios sąlygos ir taisyklės, pateikiamos Svetainėje bei nusistovėjusi gera verslo praktika. Vartotojų apsaugos reikalavimai netaikomi sutarčiai, kai prekes, paslaugas siūlantis asmuo nėra Verslininkas.
-                    </p>
-                    <p className="mb-4">
-                    1.7. Jeigu kuri nors šių Taisyklių nuostata prieštarauja įstatymams arba dėl kokios nors priežasties tampa dalinai arba visai negaliojančia, ji nedaro negaliojančiomis likusių Taisyklių nuostatų.
-                    </p>
-                    <h2 className="text-xl font-semibold mb-2">2. BENDROSIOS SĄLYGOS</h2>
-                    <p className="mb-4">
-                    2.1. neskelbiu.LT suteikia Klientams virtualią erdvę, t. y. Svetainę, kurioje Klientai turi galimybę paskelbti savo skelbimus apie siūlomas ar ieškomas prekes ar paslaugas (toliau – Skelbimai), skaityti Skelbimus, ieškoti Skelbimų, rašyti po Skelbimais komentarus ir tiesiogines žinutes bei naudotis kitomis Paslaugomis.
-                    </p>
-                    <p className="mb-4">
-                    2.2. Klientų keliami Skelbimai Svetainėje yra aiškiai atskirti: Privačių klientų Skelbimai yra pažymėti „Privataus asmens skelbimas“ kontaktų skiltyje, o Verslininkų skelbimai yra pažymėti „Verslo kliento skelbimas“.
-                    </p>
-                    <p className="mb-4">
-                    2.3. Klientas pripažįsta ir sutinka, kad jis pats, o ne neskelbiu.LT, yra atsakingas už visą Turinį, kurį gauna, siunčia ar bet kuriais kitais būdais perduoda naudodamasis Svetaine ir (ar) Paslaugomis. Turinys šiose Taisyklėse reiškia Skelbimą, o taip pat visą ir bet kokią informaciją, įskaitant duomenis, duomenų bazes, simbolius, tekstą, fotografijas, paveikslėlius, video medžiagą, dokumentus, komentarus, žinutes, bet tuo neapsiribojant, kurią Klientas pateikia, gauna, saugo, siunčia, priima ar perduoda naudodamasis Paslaugomis ir (ar) Svetaine (toliau – Turinys).
-                    </p>
-                    <p className="mb-4">
-                    2.4. Klientas supranta ir sutinka su tuo, jog neskelbiu.LT nekontroliuoja kitų Svetainės naudotojų, lankytojų ar trečiųjų šalių Turinio, kurį Klientas gali gauti, perskaityti ar kitaip sužinoti naudodamasis Svetaine ar Paslaugomis, todėl neskelbiu.LT nebus atsakingas dėl tokio Turinio tikslumo, vientisumo ir (ar) kokybės.
-                    </p>
-                    <p className="mb-4">
-                    2.5. Klientas supranta ir sutinka su tuo, kad neskelbiu.LT turi teisę, tačiau ne pareigą, savo nuožiūra tikrinti Kliento gaunamą ir perduodamą Turinį, siekiant aptikti neskelbiu.LT nepageidaujamu ar pažeidžiančiu šias Taisykles pripažįstamą Turinį, o tokį Turinį aptikus – jį šalinti ar blokuoti tokio Turinio bet kokį perdavimą.
-                    </p>
-                    <p className="mb-4">
-                    2.6. neskelbiu.LT jokiu būdu nedalyvaus pirkimo-pardavimo ar kitokiuose su Svetainėje siūlomomis prekėmis ir (ar) paslaugomis ar Skelbimais susijusiuose sandoriuose tarp Klientų (nei kaip tarpininkas, įgaliotas asmuo, atstovas ar kaip kitaip). neskelbiu.LT nebus Kliento agentu, atstovu, tarpininku ir kt.
-                    </p>
-                    <p className="mb-4">
-                    2.7. Bet koks informacijos paskelbimas ir (ar) padarymas viešai prieinama Svetainėje nėra laikoma neskelbiu.LT pasiūlymu (oferta) sudaryti sandorį.
-                    </p>
-                    <p className="mb-4">
-                    2.8. Kliento sudaryta Sutartis dėl Skelbimo patalpinimo Svetainėje yra terminuota, priklausomai nuo Skelbimo kategorijos: Skelbimai daugumoje kategorijų galioja 60 dienų (drabužių / avalynės kategorijoje - 90 dienų, ilgalaikės namų, butų ir kambarių nuomos kategorijose - 40 dienų, kolekcionavimo ir dovanojimo kategorijose - 30 dienų). Anksčiau nutraukti sutartį Klientas gali savo iniciatyva ištrindamas Skelbimą, tačiau pinigai už neišnaudotą Skelbimo talpinimo laiką nebus grąžinami. Daugiau informacijos skaitykite Taisyklių 6.6. punkte.
-                    </p>
-                    <p className="mb-4">
-                    2.9. Visi pranešimai ir informacija tarp Kliento ir neskelbiu.LT bus siunčiama elektroninėmis priemonėmis adresais, nurodytais šiose Taisyklėse arba Svetainėje, nebent šiose Taisyklėse nurodyta kitaip.
-                    </p>
-                </div>
-         </div>
-      </Layout>
-  )
+      <div className="flex-colo gap-8 w-full min-h-screen text-text lg:py-20 py-10 px-6">
+        <div className="mx-auto bg-subMain p-6 shadow-md rounded-lg">
+          <h1 className="text-2xl font-bold mb-4">{t("rulesFrame.1")}</h1>
+          <h2 className="text-xl font-semibold mb-2">{t("rulesFrame.2")}</h2>
+          <p className="mb-4">{t("rulesFrame.3")}</p>
+          <p className="mb-4">{t("rulesFrame.4")}</p>
+          <p className="mb-4">{t("rulesFrame.5")}</p>
+          <p className="mb-4">{t("rulesFrame.6")}</p>
+          <p className="mb-4">{t("rulesFrame.7")} </p>
+          <p className="mb-4">{t("rulesFrame.8")} </p>
+          <p className="mb-4">{t("rulesFrame.9")}</p>
+          <p className="mb-4">{t("rulesFrame.10")}</p>
+          <p className="mb-4">{t("rulesFrame.11")}</p>
+          <h2 className="text-xl font-semibold mb-2">{t("rulesFrame.12")}</h2>
+          <p className="mb-4">{t("rulesFrame.13")}</p>
+          <p className="mb-4">{t("rulesFrame.14")}</p>
+          <p className="mb-4">{t("rulesFrame.15")} </p>
+          <p className="mb-4">{t("rulesFrame.16")} </p>
+          <p className="mb-4">{t("rulesFrame.17")}</p>
+          <p className="mb-4">{t("rulesFrame.18")}</p>
+          <p className="mb-4">{t("rulesFrame.19")} </p>
+          <p className="mb-4">{t("rulesFrame.20")} </p>
+          <p className="mb-4">{t("rulesFrame.21")} </p>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export default Taisykles
+export default Taisykles;
