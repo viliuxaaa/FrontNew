86fc391c9fd9928805c76df21731ac152eee25ba
Dalius <dalius.rac@gmail.com>
2023-08-14 11:49:16 +0300

    Isversta changePassword, allPosters, myPosters, myPosterTable

--- src/Components/PosterMaxDetail.js
+++ src/Components/PosterMaxDetail.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom"
+import { useTranslation } from "react-i18next";
 
 function PosterMaxDetail({poster}) {
 
+    const [t, i18n] = useTranslation("global");
     const getPosterImg = `/api/v1/images/poster/get/${poster.posterId}/0`
     // mazinam title teksta
     let shortTitle = poster.postName.substring(0, 40);
@@ -66,18 +68,20 @@ function PosterMaxDetail({poster}) {
                                 className="w-40 h-40 border-[2px] border-darkAccent shadow-md rounded-l-xl"
                             />
                     </div>
-                    <div className="xs:w-[400px] md:w-[800px] border-[2px] border-main xs:p-1 h-40 p-2 shadow-md bg-subMain rounded-r-lg">
+                    <div className="xs:w-[400px] md:w-[800px] border-[2px]  border-main xs:p-1 h-40 p-2 shadow-md bg-subMain rounded-r-lg">
                         <h2 className="xs:text-base md:text-xl font-semibold">{titleToDisplay}</h2>
                         <p className="">
                             {descToDisplay}                     
                         </p>
                         <br/>
                         <p className="pt-3 align-text-bottom sm:text-sm xs:text-base md:text-base float-right">
-                            Kaina: {poster?.price} &euro;
+                        {t("allPosters.price")} {poster?.price} &euro;
                         </p>
                         <p className="pt-3 align-text-bottom sm:text-sm xs:text-base md:text-base">
                             {poster?.city}
-                        </p>                
+                        </p>   
+                        
+                                     
                     </div>
                 </Link>    
             </div>
