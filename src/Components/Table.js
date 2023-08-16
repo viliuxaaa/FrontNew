02ee0671feb3918c529ed10ed5f036397f022722
ed <stanke367@gmail.com>
2023-08-16 09:54:10 +0300

    error fixed on npm start

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom'
 import { GoEye } from 'react-icons/go'
 import { FiSettings } from 'react-icons/fi'
 import useAuth from '../hooks/useAuth'
-import useAxiosPrivate from '../hooks/useAxiosPrivate';
+import useAxiosPrivate from '../hooks/useAxiosPrivate'
+import { useTranslation } from "react-i18next";
 import { computerAEnum as catA,
     categoryTranslationKeys as langFileStrings,
-    allArrays,
+    allArrays
   } from '../enums/AllEnumArrays';
 
 
@@ -27,7 +28,6 @@ const Rows = (poster, i, t ) => {
     const [catBArray, setCatBArray] = useState([]);
     const [catBDisplay, setCatBDisplay] = useState('');
     const [tempLangString, setTempLangString ] = useState("")
-
     const deletePost = () => {
         const controller = new AbortController();
     
@@ -55,14 +55,12 @@ const Rows = (poster, i, t ) => {
         }
         deleteThing();
     }
-
     
     useEffect(() =>{
-        translateCategories(); 
         setImg(
             {label: "Image 1", alt: "image1", url: "/api/v1/images/poster/get/"+poster?.posterId+"/" + 0}
         );
-        
+        translateCategories(); 
     }, [])
 
     function translateCategories(){
@@ -77,11 +75,11 @@ const Rows = (poster, i, t ) => {
         for(let j=0 ;j<catBArray.length; j++){
             if ( catBArray[j] === poster?.categoryB ){
                 setCatBDisplay(t(tempLangString+"."+j))
-                
+                console.log(t(tempLangString+"."+j))
             }
         }
 
-        // console.log(poster?.categoryB)
+        console.log(poster?.categoryB)
     }
     
       // mazinam title teksta
@@ -125,7 +123,7 @@ const Rows = (poster, i, t ) => {
                                 <MdDelete />
                             </button>
                             <Link 
-                                to={`/skelbimas/${poster?.posterId}`} 
+                                to={`/skelbimas/${poster?.name}`} 
                                 className='bg-subMain border hover:bg-purple-400 border-text text-text rounded flex-colo w-7 h-7'
                             >
                                 <GoEye />
