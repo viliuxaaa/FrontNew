ec3d0555599c50943c7be4d728d007a0c4d87205
ed <stanke367@gmail.com>
2023-08-18 08:30:44 +0300

    table fix

--- src/Components/Single/PosterInfo.js
+++ src/Components/Single/PosterInfo.js
@@ -23,43 +23,45 @@ function PosterInfo({poster, t}) {
             { +auth.userId === poster.userId && <PosterMenu id={poster.posterId}/>}
             {/* ////////////////////////////// */}
             <div className="bg-main border-[2px] border-darkMain container shadow-xl rounded-xl font-semibold text-3xl w-full mt-2 mx-auto px-2 py-2 xs:pt-7 xs:pb-3 lg:py-6">
-            <table class="bg-main border-2 border-darkMain container font-medium text-3xl w-full mx-auto px-5 py-2 xs:pt-7 xs:pb-3 lg:py-6">
-                <tr class="bg-subMain h-fit p-3 border-b-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.tel")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">+{poster.phoneNumber}</span></td>
-                </tr>
-                <tr class="bg-subMain h-fit p-3 border-t-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.miestas")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">{poster.city}</span></td>
-                </tr>
-                <tr class="bg-subMain h-fit p-3 border-t-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.kat")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">{poster.categoryA}</span></td>
-                </tr>
-                <tr class="bg-subMain h-fit p-3 border-t-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.status")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">{poster.status}</span></td>
-                </tr>
-                <tr class="bg-subMain h-fit p-3 border-t-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.website")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">{poster.website ? poster.website : '-'}</span></td>
-                </tr>
-                <tr class="bg-subMain h-fit p-3 border-t-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.video")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">{poster.videolink ? poster.videolink : '-'}</span></td>
-                </tr>
-                <tr class="bg-subMain rounded-b-xl h-fit p-3 border-y-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.sukurtas")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">{dateCreated ? dateCreated : '-'}</span></td>
-                </tr>
-                <tr class="bg-subMain rounded-b-xl h-fit p-3 border-y-2 border-darkMain">
-                    <td class="text-base md:text-2xl px-4">{t("single.atnaujintas")}</td>
-                    <td class="text-base md:text-2xl flex"><span class="font-normal">{dateUpdated ? dateUpdated : '-'}</span></td>
-                </tr>
-                <tr class="bg-subMain h-[60px] p-3">
-                    <td class="text-base md:text-2xl px-4">{t("single.kaina")}</td>
-                    <td class="text-6xl flex pt-2"><span class="font-normal text-green-500">{poster.price}</span> <TbCurrencyEuro size={36}/></td>
-                </tr>
+            <table className="bg-main border-2 border-darkMain container font-medium text-3xl w-full mx-auto px-5 py-2 xs:pt-7 xs:pb-3 lg:py-6">
+                <tbody>
+                    <tr className="bg-subMain h-fit p-3 border-b-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.tel")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">+{poster.phoneNumber}</span></td>
+                    </tr>
+                    <tr className="bg-subMain h-fit p-3 border-t-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.miestas")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">{poster.city}</span></td>
+                    </tr>
+                    <tr className="bg-subMain h-fit p-3 border-t-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.kat")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">{poster.categoryA}</span></td>
+                    </tr>
+                    <tr className="bg-subMain h-fit p-3 border-t-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.status")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">{poster.status}</span></td>
+                    </tr>
+                    <tr className="bg-subMain h-fit p-3 border-t-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.website")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">{poster.website ? poster.website : '-'}</span></td>
+                    </tr>
+                    <tr className="bg-subMain h-fit p-3 border-t-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.video")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">{poster.videolink ? poster.videolink : '-'}</span></td>
+                    </tr>
+                    <tr className="bg-subMain rounded-b-xl h-fit p-3 border-y-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.sukurtas")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">{dateCreated ? dateCreated : '-'}</span></td>
+                    </tr>
+                    <tr className="bg-subMain rounded-b-xl h-fit p-3 border-y-2 border-darkMain">
+                        <td className="text-base md:text-2xl px-4">{t("single.atnaujintas")}</td>
+                        <td className="text-base md:text-2xl flex"><span className="font-normal">{dateUpdated ? dateUpdated : '-'}</span></td>
+                    </tr>
+                    <tr className="bg-subMain h-[60px] p-3">
+                        <td className="text-base md:text-2xl px-4">{t("single.kaina")}</td>
+                        <td className="text-6xl flex pt-2"><span className="font-normal text-green-500">{poster.price}</span> <TbCurrencyEuro size={36}/></td>
+                    </tr>
+                </tbody>
             </table>
             </div>
             {/* /////////////////// */}
